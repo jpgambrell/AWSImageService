97f434c6e2e9e24cc7286488dfa1a1af1226e9c1
jpgambrell <john.gambrell@gmail.com>
2026-01-16 11:58:13 -0600

    Initial commit

---
+++ test/aws_image_service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsImageService from '../lib/aws_image_service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_image_service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsImageService.AwsImageServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
